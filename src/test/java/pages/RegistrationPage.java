0a26c10cb4aad0bd019ccca197e0f6a4ca0d45d9
Sergei Volkov <Sneedjik@gmail.com>
2024-10-08 19:54:58 +0600

    added OPM jpg

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -2,14 +2,17 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
 
 
     public void openPage() {
-        open("automation-practice-form");
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
     }
 
     public static SelenideElement firstNameLocator = $("#firstName");
